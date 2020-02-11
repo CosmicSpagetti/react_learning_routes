4a21b959506a2ed20245dbec52fa34e866157396
Billy <billyurrutia@gmail.com>
2020-02-11 10:14:37 -0700

    Create route for Home page and links to future components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, NavLink } from 'react-router-dom'
+import Home from './Home'
+
 
 export default class App extends Component {
   render() {
     return (
       <main className="App">
-        Hello World!
+        <header>
+          <NavLink to='/unicorns' className='nav'>Unicorns</NavLink>
+          <NavLink to='/puppies' className='nav'>Puppies</NavLink>
+          <NavLink to='/sharks' className='nav'>Sharks</NavLink>
+        </header>
+        <Route path='/' component={Home} />
       </main>
     );
   }

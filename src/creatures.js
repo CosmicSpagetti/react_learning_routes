170828be48b4860858fb035afcb0b7c95413ae5f
Billy <billyurrutia@gmail.com>
2020-02-18 11:29:00 -0700

    created creatures page that will display all creatures instead of individual pages

---
+++ src/creatures.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './image-display.css'
+
+const Creatures = ({data}) => {
+  const displayCreatures = data.map(creature => {
+    const { id, image } = creature; 
+    return <img src={image} className='app-img' key={id} />
+  })
+  return (
+    <>
+    <h1>Creatures!</h1>
+    {displayCreatures}
+    </>
+  )
+}
+
+export default Creatures;

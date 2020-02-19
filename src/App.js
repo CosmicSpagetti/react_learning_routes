97e675bd8d975a342e87e5f69a320ab3f969cd62
Billy <billyurrutia@gmail.com>
2020-02-19 08:02:59 -0700

    add dynamic routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Creatures from './creatures'
 import unicornData from './data/unicorn-data';
 import sharkData from './data/shark-data';
 import puppyData from './data/puppy-data'
+import CreatureDetails from './CreatureDetails'
+
 export default class App extends Component {
   render() {
     return (
@@ -16,9 +18,24 @@ export default class App extends Component {
           <NavLink to='/sharks' className='nav'>Sharks</NavLink>
         </header>
         <Route exact path='/' component={Home} />
-        <Route path='/unicorns' render={() => <Creatures data={unicornData} />} />
-        <Route path='/puppies' render={() => <Creatures data={puppyData} />} />
-        <Route path='/sharks' render={() => <Creatures data={sharkData} />} />
+        <Route exact path='/unicorns' render={() => <Creatures data={unicornData} />} />
+        <Route exact path='/puppies' render={() => <Creatures data={puppyData} />} />
+        <Route exact path='/sharks' render={() => <Creatures data={sharkData} />} />
+        <Route path='/unicorns/:id' render={({ match }) => {
+          const { id } = match.params
+          const creature = unicornData.find(unicorn => unicorn.id === parseInt(id))
+          return creature && <CreatureDetails {...creature} />
+        }} />
+        <Route path='/puppies/:id' render={({ match }) => {
+          const { id } = match.params
+          const creature = puppyData.find(puppy => puppy.id === parseInt(id))
+          return creature && <CreatureDetails {...creature} />
+        }} />
+        <Route path='/sharks/:id' render={({ match }) => {
+          const { id } = match.params
+          const creature = sharkData.find(shark => shark.id === parseInt(id))
+          return creature && <CreatureDetails {...creature} />
+        }} />
       </main>
     );
   }
